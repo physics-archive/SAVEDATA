c70637040d84f8f3196dc2258425f9a30aab9782
physics-archive <windows.client>
2024-03-04 19:24:14 -0500

    xsave

--- Tools/Diablo/robosave/robosave.bat
+++ Tools/Diablo/robosave/robosave.bat
@@ -2,4 +2,8 @@ pushd "C:\Program Files (x86)\Diablo"
 
 1>>%~dp0Log.txt xcopy *.sv %USERPROFILE%\PROJ\SAVEDATA\Diablo\xsave\
 popd
+
+1>>%~dp0Log.txt echo.
+1>>%~dp0Log.txt ###############COMPLETE###############
+1>>%~dp0Log.txt echo.
 pause
